60315479c693bd170fdc17b3bdc040d76d22f9e7
chaoyu <chaoyu9225@gmail.com>
2025-07-11 20:39:00 +0800

    调整nvim配色

--- init.lua
+++ init.lua
@@ -9,6 +9,10 @@ vim.opt.splitbelow = true          -- 水平分割窗口在下方
 
 vim.o.mouse = ""
 
+-- 修改nvim配色
+vim.cmd[[colorscheme vim]]
+vim.o.termguicolors = false
+
 -- 安装 lazy.nvim 插件管理器
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
