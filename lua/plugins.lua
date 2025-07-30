f2df60361f744fc72f0d74c4f8a568f020cccc9c
chaoyu <chaoyu9225@gmail.com>
2025-07-30 10:28:45 +0800

    调整plugins.lua中插件的顺序

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,72 +1,5 @@
 -- 插件配置
 
--- Tagbar 插件，用于显示代码大纲
-local tagbar = {
-  'preservim/tagbar',
-  keys = {
-    { '<F2>', '<cmd>TagbarToggle<cr>', desc = '切换标签栏' },
-    { '<F4>', '<cmd>TagbarForceUpdate<cr>', desc = '强制更新标签栏' }
-  },
-  config = function()
-    -- Tagbar 配置
-    vim.g.tagbar_width = 30
-    vim.g.tagbar_autofocus = 0
-    vim.g.tagbar_sort = 0
-    -- 在左侧显示
-    vim.g.tagbar_position = 'topleft vertical'
-  end
-}
-
--- NvimTree 文件浏览器
-local nvimtree = {
-  'nvim-tree/nvim-tree.lua',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons', -- 文件图标
-  },
-  keys = {
-    { '<F9>', '<cmd>NvimTreeToggle<cr>', desc = '切换文件浏览器' }
-  },
-  config = function()
-    -- 必须先禁用netrw
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-
-    require('nvim-tree').setup({
-      view = {
-        side = 'right',
-        width = 30,
-      },
-      filters = {
-        dotfiles = false,
-      },
-      git = {
-        enable = true,
-        ignore = false,
-      },
-      renderer = {
-        highlight_git = true,
-        icons = {
-          show = {
-            git = false,        -- 禁用git图标
-            file = false,       -- 禁用文件图标
-            folder = false,     -- 禁用文件夹图标
-            folder_arrow = true, -- 保留文件夹箭头
-          },
-          glyphs = {
-            folder = {
-              arrow_closed = "+",  -- 使用+号表示可展开
-              arrow_open = "-",    -- 使用-号表示可收起
-            },
-          },
-        },
-        indent_markers = {
-          enable = false,       -- 禁用缩进标记
-        },
-      },
-    })
-  end
-}
-
 -- 自动配对括号
 local auto_pairs = {
   'jiangmiao/auto-pairs',
@@ -117,188 +50,6 @@ local auto_pairs = {
         endif
       endfunction
     ]])
-  end
-}
-
--- 文本绘图工具
-local drawit = {
-  'vim-scripts/DrawIt'
-}
-
--- Markdown 语法高亮和功能增强
-local vim_markdown = {
-  'preservim/vim-markdown',
-  ft = 'markdown',
-  config = function()
-    -- 禁用折叠功能
-    vim.g.vim_markdown_folding_disabled = 1
-  end
-}
-
--- Markdown 预览插件
-local markdown_preview = {
-  'iamcco/markdown-preview.nvim',
-  build = 'cd app && npm install',
-  ft = 'markdown',
-  keys = {
-    { '<F5>', '<cmd>MarkdownPreviewToggle<cr>', desc = '切换Markdown预览' }
-  },
-  config = function()
-    -- 设置浏览器
-    vim.g.mkdp_browser = ''
-    -- 预览页面标题
-    vim.g.mkdp_page_title = '${name}'
-    -- 只在markdown文件中启用
-    vim.g.mkdp_filetypes = {'markdown'}
-  end
-}
-
--- 表格模式插件
-local table_mode = {
-  'dhruvasagar/vim-table-mode',
-  ft = 'markdown',
-  config = function()
-    -- 设置表格分隔符
-    vim.g.table_mode_corner = '|'
-    -- 启用表格模式快捷键
-    vim.g.table_mode_map_prefix = '<Leader>t'
-  end
-}
-
--- 自动tags和cscope管理
-local gutentags = {
-  'ludovicchabant/vim-gutentags',
-  config = function()
-    -- 设置tags缓存目录
-    vim.g.gutentags_cache_dir = vim.fn.expand("~/.cache/gutentags")
-
-    -- 项目根目录标记
-    vim.g.gutentags_project_root = {'.root', '.svn', '.git', '.hg', '.project'}
-
-    -- 启用模块（仅tags，Neovim不支持cscope）
-    vim.g.gutentags_modules = {'ctags'}
-
-    -- tags文件名
-    vim.g.gutentags_ctags_tagfile = 'tags'
-
-    -- 排除不需要生成tags的文件和目录
-    vim.g.gutentags_ctags_exclude = {
-      '*.git', '*.svg', '*.hg',
-      '*/tests/*',
-      'build',
-      'dist',
-      'sites/*/files/*',
-      'bin',
-      'node_modules',
-      'bower_components',
-      'cache',
-      'compiled',
-      'docs',
-      'example',
-      'bundle',
-      'vendor',
-      '*.md',
-      '*-lock.json',
-      '*.lock',
-      '*bundle*.js',
-      '*build*.js',
-      '.*rc*',
-      '*.json',
-      '*.min.*',
-      '*.map',
-      '*.bak',
-      '*.zip',
-      '*.pyc',
-      '*.class',
-      '*.o',
-      '*.o.*',
-      '*.a',
-      '*.s',
-      '*.ko',
-      '*.so',
-      '*.so.dbg',
-      '*.mod.c',
-      '*.i',
-      '*.lst',
-      '*.symtypes',
-      '*.order',
-      '*.elf',
-      '*.bin',
-      '*.gcno',
-      '*.ll',
-      '*.dwo',
-      '*.su',
-      'modules.builtin',
-      'Module.symvers',
-      '*.sln',
-      '*.tmp',
-      '*.cache',
-      '*.pdb',
-      'tags*',
-      'TAGS',
-      'cscope.*',
-      'ncscope.*',
-      'GPATH',
-      'GRTAGS',
-      'GSYMS',
-      'GTAGS',
-      'ID',
-      '*.orig',
-      '*.css',
-      '*.less',
-      '*.scss',
-      '*.exe', '*.dll',
-      '*.swp', '*.swo',
-      '*.bmp', '*.gif', '*.ico', '*.jpg', '*.png',
-      '*.rar', '*.zip', '*.tar', '*.tar.gz', '*.tar.xz', '*.tar.bz2',
-      '*.lzma', '*.lz4', '*.lzo', '*.bz2',
-      '*.patch',
-      '*.pdf', '*.doc', '*.docx', '*.ppt', '*.pptx',
-      '*.kdev4',
-    }
-
-    -- 在状态栏文件名后添加gutentags状态
-    local current_statusline = vim.o.statusline
-    if current_statusline == "" then
-      -- 如果没有自定义状态栏，使用默认格式并在文件名后添加[ctags]状态
-      vim.opt.statusline = "%<%f%{gutentags#statusline() != '' ? '[ctags]' : ''} %h%m%r%=%-14.(%l,%c%V%) %P"
-    else
-      -- 如果已有自定义状态栏，智能插入[ctags]到文件名后
-      local modified_statusline = current_statusline
-      -- 查找文件名模式并在其后插入gutentags状态
-      if string.find(current_statusline, "%%f") then
-        modified_statusline = string.gsub(current_statusline, "(%%<?f)", "%1%%{gutentags#statusline() != '' ? '[ctags]' : ''}")
-      elseif string.find(current_statusline, "%%t") then
-        modified_statusline = string.gsub(current_statusline, "(%%t)", "%1%%{gutentags#statusline() != '' ? '[ctags]' : ''}")
-      else
-        -- 如果找不到文件名模式，追加到末尾
-        modified_statusline = current_statusline .. "%{gutentags#statusline() != '' ? ' [ctags]' : ''}"
-      end
-      vim.opt.statusline = modified_statusline
-    end
-  end
-}
-
--- Cscope集成
-local cscope_maps = {
-  'dhananjaylatkar/cscope_maps.nvim',
-  dependencies = {
-    "folke/trouble.nvim", -- 可选，用于显示搜索结果
-    "nvim-telescope/telescope.nvim", -- 可选，用于更好的搜索体验
-  },
-  config = function()
-    require("cscope_maps").setup({
-      disable_maps = false,
-      skip_input_prompt = false,
-      cscope = {
-        db_file = "./cscope.out",
-        exec = "gtags-cscope",
-        picker = "quickfix", -- 可选: "quickfix", "telescope", "trouble"
-        skip_input_prompt = false,
-        db_build_cmd = { scripts = "cscope -Rbkq" },
-        statusline_indicator = nil,
-      }
-    })
   end
 }
 
@@ -421,6 +172,148 @@ local claudecode = {
   },
 }
 
+-- Cscope集成
+local cscope_maps = {
+  'dhananjaylatkar/cscope_maps.nvim',
+  dependencies = {
+    "folke/trouble.nvim", -- 可选，用于显示搜索结果
+    "nvim-telescope/telescope.nvim", -- 可选，用于更好的搜索体验
+  },
+  config = function()
+    require("cscope_maps").setup({
+      disable_maps = false,
+      skip_input_prompt = false,
+      cscope = {
+        db_file = "./cscope.out",
+        exec = "gtags-cscope",
+        picker = "quickfix", -- 可选: "quickfix", "telescope", "trouble"
+        skip_input_prompt = false,
+        db_build_cmd = { scripts = "cscope -Rbkq" },
+        statusline_indicator = nil,
+      }
+    })
+  end
+}
+
+-- 文本绘图工具
+local drawit = {
+  'vim-scripts/DrawIt'
+}
+
+-- 自动tags和cscope管理
+local gutentags = {
+  'ludovicchabant/vim-gutentags',
+  config = function()
+    -- 设置tags缓存目录
+    vim.g.gutentags_cache_dir = vim.fn.expand("~/.cache/gutentags")
+
+    -- 项目根目录标记
+    vim.g.gutentags_project_root = {'.root', '.svn', '.git', '.hg', '.project'}
+
+    -- 启用模块（仅tags，Neovim不支持cscope）
+    vim.g.gutentags_modules = {'ctags'}
+
+    -- tags文件名
+    vim.g.gutentags_ctags_tagfile = 'tags'
+
+    -- 排除不需要生成tags的文件和目录
+    vim.g.gutentags_ctags_exclude = {
+      '*.git', '*.svg', '*.hg',
+      '*/tests/*',
+      'build',
+      'dist',
+      'sites/*/files/*',
+      'bin',
+      'node_modules',
+      'bower_components',
+      'cache',
+      'compiled',
+      'docs',
+      'example',
+      'bundle',
+      'vendor',
+      '*.md',
+      '*-lock.json',
+      '*.lock',
+      '*bundle*.js',
+      '*build*.js',
+      '.*rc*',
+      '*.json',
+      '*.min.*',
+      '*.map',
+      '*.bak',
+      '*.zip',
+      '*.pyc',
+      '*.class',
+      '*.o',
+      '*.o.*',
+      '*.a',
+      '*.s',
+      '*.ko',
+      '*.so',
+      '*.so.dbg',
+      '*.mod.c',
+      '*.i',
+      '*.lst',
+      '*.symtypes',
+      '*.order',
+      '*.elf',
+      '*.bin',
+      '*.gcno',
+      '*.ll',
+      '*.dwo',
+      '*.su',
+      'modules.builtin',
+      'Module.symvers',
+      '*.sln',
+      '*.tmp',
+      '*.cache',
+      '*.pdb',
+      'tags*',
+      'TAGS',
+      'cscope.*',
+      'ncscope.*',
+      'GPATH',
+      'GRTAGS',
+      'GSYMS',
+      'GTAGS',
+      'ID',
+      '*.orig',
+      '*.css',
+      '*.less',
+      '*.scss',
+      '*.exe', '*.dll',
+      '*.swp', '*.swo',
+      '*.bmp', '*.gif', '*.ico', '*.jpg', '*.png',
+      '*.rar', '*.zip', '*.tar', '*.tar.gz', '*.tar.xz', '*.tar.bz2',
+      '*.lzma', '*.lz4', '*.lzo', '*.bz2',
+      '*.patch',
+      '*.pdf', '*.doc', '*.docx', '*.ppt', '*.pptx',
+      '*.kdev4',
+    }
+
+    -- 在状态栏文件名后添加gutentags状态
+    local current_statusline = vim.o.statusline
+    if current_statusline == "" then
+      -- 如果没有自定义状态栏，使用默认格式并在文件名后添加[ctags]状态
+      vim.opt.statusline = "%<%f%{gutentags#statusline() != '' ? '[ctags]' : ''} %h%m%r%=%-14.(%l,%c%V%) %P"
+    else
+      -- 如果已有自定义状态栏，智能插入[ctags]到文件名后
+      local modified_statusline = current_statusline
+      -- 查找文件名模式并在其后插入gutentags状态
+      if string.find(current_statusline, "%%f") then
+        modified_statusline = string.gsub(current_statusline, "(%%<?f)", "%1%%{gutentags#statusline() != '' ? '[ctags]' : ''}")
+      elseif string.find(current_statusline, "%%t") then
+        modified_statusline = string.gsub(current_statusline, "(%%t)", "%1%%{gutentags#statusline() != '' ? '[ctags]' : ''}")
+      else
+        -- 如果找不到文件名模式，追加到末尾
+        modified_statusline = current_statusline .. "%{gutentags#statusline() != '' ? ' [ctags]' : ''}"
+      end
+      vim.opt.statusline = modified_statusline
+    end
+  end
+}
+
 local luaSnip = {
 	"L3MON4D3/LuaSnip",
 	version = "v2.4.0",
@@ -449,16 +342,123 @@ local luaSnip = {
 	end
 }
 
+-- NvimTree 文件浏览器
+local nvimtree = {
+  'nvim-tree/nvim-tree.lua',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons', -- 文件图标
+  },
+  keys = {
+    { '<F9>', '<cmd>NvimTreeToggle<cr>', desc = '切换文件浏览器' }
+  },
+  config = function()
+    -- 必须先禁用netrw
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    require('nvim-tree').setup({
+      view = {
+        side = 'right',
+        width = 30,
+      },
+      filters = {
+        dotfiles = false,
+      },
+      git = {
+        enable = true,
+        ignore = false,
+      },
+      renderer = {
+        highlight_git = true,
+        icons = {
+          show = {
+            git = false,        -- 禁用git图标
+            file = false,       -- 禁用文件图标
+            folder = false,     -- 禁用文件夹图标
+            folder_arrow = true, -- 保留文件夹箭头
+          },
+          glyphs = {
+            folder = {
+              arrow_closed = "+",  -- 使用+号表示可展开
+              arrow_open = "-",    -- 使用-号表示可收起
+            },
+          },
+        },
+        indent_markers = {
+          enable = false,       -- 禁用缩进标记
+        },
+      },
+    })
+  end
+}
+
+-- Markdown 预览插件
+local markdown_preview = {
+  'iamcco/markdown-preview.nvim',
+  build = 'cd app && npm install',
+  ft = 'markdown',
+  keys = {
+    { '<F5>', '<cmd>MarkdownPreviewToggle<cr>', desc = '切换Markdown预览' }
+  },
+  config = function()
+    -- 设置浏览器
+    vim.g.mkdp_browser = ''
+    -- 预览页面标题
+    vim.g.mkdp_page_title = '${name}'
+    -- 只在markdown文件中启用
+    vim.g.mkdp_filetypes = {'markdown'}
+  end
+}
+
+-- 表格模式插件
+local table_mode = {
+  'dhruvasagar/vim-table-mode',
+  ft = 'markdown',
+  config = function()
+    -- 设置表格分隔符
+    vim.g.table_mode_corner = '|'
+    -- 启用表格模式快捷键
+    vim.g.table_mode_map_prefix = '<Leader>t'
+  end
+}
+
+-- Tagbar 插件，用于显示代码大纲
+local tagbar = {
+  'preservim/tagbar',
+  keys = {
+    { '<F2>', '<cmd>TagbarToggle<cr>', desc = '切换标签栏' },
+    { '<F4>', '<cmd>TagbarForceUpdate<cr>', desc = '强制更新标签栏' }
+  },
+  config = function()
+    -- Tagbar 配置
+    vim.g.tagbar_width = 30
+    vim.g.tagbar_autofocus = 0
+    vim.g.tagbar_sort = 0
+    -- 在左侧显示
+    vim.g.tagbar_position = 'topleft vertical'
+  end
+}
+
+-- Markdown 语法高亮和功能增强
+local vim_markdown = {
+  'preservim/vim-markdown',
+  ft = 'markdown',
+  config = function()
+    -- 禁用折叠功能
+    vim.g.vim_markdown_folding_disabled = 1
+  end
+}
+
 return {
-  tagbar,
-  nvimtree,
   auto_pairs,
+  claudecode,
+--  cscope_maps,
   drawit,
-  vim_markdown,
+  gutentags,
+  luaSnip,
+  nvimtree,
   markdown_preview,
   table_mode,
-  gutentags,
---  cscope_maps,
-  claudecode,
-  luaSnip
+  tagbar,
+  vim_markdown
 }
