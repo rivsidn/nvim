3e23a50bd2ff87ac9d21e98aa0f7380adf1d624d
chaoyu <chaoyu9225@gmail.com>
2025-08-22 15:13:57 +0800

    enter claude code terminal when send buffer

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -158,8 +158,18 @@ local claudecode = {
     { "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
     { "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
     { "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
-    { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
-    { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+    { "<leader>as", "<cmd>ClaudeCodeAdd %<cr><cmd>ClaudeCodeFocus<cr>", mode="n", desc = "Add current buffer" },
+    {
+      "<leader>as",
+      function()
+        vim.cmd('ClaudeCodeSend')
+        vim.schedule(function()
+          vim.cmd('ClaudeCodeFocus')
+        end)
+      end,
+      mode = "v",
+      desc = "Send to Claude"
+    },
     {
       "<leader>as",
       "<cmd>ClaudeCodeTreeAdd<cr>",
@@ -195,7 +205,7 @@ local cscope_maps = {
         qf_window_size = 8,  -- 设置quickfix窗口高度为25行
       }
     })
-    
+
     -- cscope打开quickfix后自动调整高度
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "qf",
