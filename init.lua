4b2d3935cff75dd8c6f12718973450b70eb233ef
chaoyu <chaoyu9225@gmail.com>
2026-03-20 20:54:43 +0800

    配置nvim 粘贴板.

--- init.lua
+++ init.lua
@@ -10,6 +10,18 @@ vim.opt.splitbelow = true          -- 水平分割窗口在下方
 
 vim.o.mouse = ""
 
+-- 可用时默认使用系统剪贴板，省去每次手动输入 "+ 寄存器
+local has_clipboard_provider =
+  (vim.env.WAYLAND_DISPLAY and vim.fn.executable("wl-copy") == 1)
+  or (vim.env.DISPLAY and (vim.fn.executable("xclip") == 1 or vim.fn.executable("xsel") == 1))
+  or vim.fn.executable("pbcopy") == 1
+  or vim.fn.executable("clip.exe") == 1
+  or vim.fn.executable("win32yank.exe") == 1
+
+if has_clipboard_provider then
+  vim.opt.clipboard:append("unnamedplus")
+end
+
 -- 修改nvim配色
 -- 如果此处报错，删除掉apt 安装的neovim
 -- 执行snap install nvim 命令安装
@@ -48,4 +60,3 @@ vim.keymap.set('i', '<C-@>', '<Nop>', {desc = '禁用Ctrl+@重复插入'})
 --     end
 --   end,
 -- })
-
